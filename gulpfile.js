366b3166d2245e908307062e69c04d1d40ad6766
John Sparwasser <johnspar1@gmail.com>
2017-01-02 12:13:34 -0500

    Button work

---
+++ gulpfile.js
@@ -0,0 +1,14 @@
+var gulp = require("gulp");
+var sass = require("gulp-sass");
+
+gulp.task('sass', function() {
+    return gulp.src('./scss/includes.scss')
+        .pipe(sass().on('error', sass.logError))
+        .pipe(gulp.dest('./compiled'));
+});
+
+gulp.task('default', ['sass'], function() {
+    gulp.watch('./scss/*.scss', ['sass']);
+    gulp.watch('./scss/_*.scss', ['sass']);
+    gulp.watch('./scss/**/*.scss', ['sass']);
+});
