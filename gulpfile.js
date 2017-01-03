e055af80ac44b37b55b27dbdd343ba2755cfd600
John Sparwasser <johnspar1@gmail.com>
2017-01-03 16:23:23 -0500

    Reorganized entire project and added in a constructor

--- gulpfile.js
+++ gulpfile.js
@@ -2,7 +2,7 @@ var gulp = require("gulp");
 var sass = require("gulp-sass");
 
 gulp.task('sass', function() {
-    return gulp.src('./scss/includes.scss')
+    return gulp.src('./scss/styles.scss')
         .pipe(sass().on('error', sass.logError))
         .pipe(gulp.dest('./compiled'));
 });
